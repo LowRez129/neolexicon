d4d6c5027a4ba6644a71b606e36043c9a8eaf438
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2024-03-06 10:13:24 +1300

    Rearrange grid.

--- client/src/component/App/Word.tsx
+++ client/src/component/App/Word.tsx
@@ -10,18 +10,18 @@ function Word ({ user_uuid, word, description } : WordProp) {
     }
 
     const description_container = (toggle == true) ? (
-        <>
+        <div className='view-container'>
+            <button className='word-button' onClick={toggleDescription}>{word}</button>
             <p className='description'>{description}</p>
-            <button onClick={() => window.location.href = `user/search?user=${user_uuid}`} >{user_uuid}</button>
-        </>
-    ) : <></>;
+            <button onClick={() => window.location.href = `user/search?user=${user_uuid}`} >{user_uuid}</button>    
+        </div>
+    ) : <button className='word-button' onClick={toggleDescription}>{word}</button>;
 
     const resize_column = (!toggle) ? '' : 'auto / span 2';
-    const resize_row = (!toggle) ? '' : 'auto / span 2';
+    const resize_row = (!toggle) ? '' : 'auto / span 3';
 
     return (
         <div className='word-container' style={{gridColumn: `${resize_column}`, gridRow: `${resize_row}`}}>
-            <button className='word-button' onClick={toggleDescription}>{word}</button>
             {description_container}
         </div>
     )
