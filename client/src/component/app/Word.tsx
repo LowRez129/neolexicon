ef88f37b6aad4dbaa4a53afbcf443d2c5af66dbe
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2024-03-09 16:45:57 +1300

    Recolor grid.

--- client/src/component/app/Word.tsx
+++ client/src/component/app/Word.tsx
@@ -9,14 +9,14 @@ function Word ({ username, user_uuid, word, description } : WordProp) {
         setToggle(!toggle);
     }
 
-    const word_button = <button className='word-button' onClick={toggleDescription}>{word}</button>;
     const description_container = (toggle == true) ? (
-        <div className='view-container'>
-            {word_button}
+        <div className='view'>
+            <div className='word'>{word}</div>
+            <button className='close-view' onClick={toggleDescription} >&lt;&lt;&lt;</button>
             <div className='description'>{description}</div>
             <button className='view-user-button' onClick={() => window.location.href = `user/search?user=${user_uuid}`}>{username}</button>    
         </div>
-    ) : word_button;
+    ) : <button className='word-button' onClick={toggleDescription}>{word}</button>;
 
     const resize_column = (!toggle) ? '' : 'auto / span 2';
     const resize_row = (!toggle) ? '' : 'auto / span 3';
